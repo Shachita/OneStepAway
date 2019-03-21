37173e13e6d4a637f608f9f4976f9fde94693c4b
Siddharth Varangaonkar <sidvarangaonkar1905@gmail.com>
2019-03-21 11:39:29 +0530

    second commit

--- OneStepAway/flasker.py
+++ OneStepAway/flasker.py
@@ -1,34 +1,43 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
-'''Imported for database'''
+#Imported for database
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/onestepawaydb'
 
-'''Home page'''
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    #(name, email, password, contact , service , address )
+    name=db.Column(db.Integer, primary_key=True)
+    emailid=db.Column(db.String(80),nullable=True)
+    password=db.Column(db.String(120),nullable=True)
+    contact=db.Column(db.String(80),nullable=True)
+    service=db.Column(db.String(80),nullable=True)
+    address=db.Column(db.String(80),nullable=True)
+
+    
+
 @app.route("/")
 def home():
-    return 
+    return render_template("home.html")
 
-'''registration'''  
-def registration():
-
-
-
-def login():
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route("/register",methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form.get('name', False)
+        email=request.form.get('email', False)
+        password=request.form.get('pass', False)
+        contact=request.form.get('phone', False)
+        service=request.form.get('service', False)
+        address=request.form.get('address', False)
+        entry=User(name=name, contact=contact, emailid=email,password=password, service=service, address=address )
+        db.session.add(entry)
+        db.session.commit()
+        return render_template('login.html')
+    return render_template('Registrationu.html')    
 
 
 if __name__ == "__main__":
